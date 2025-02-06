3722accd4e5d61c16db6007fdd11a3f5690d720b
Julie Delon <julie.delon@parisdescartes.fr>
2025-02-06 21:04:41 +0100

    updated p_norm_q_barycentres

--- examples/2d_p_norm_q/src.py
+++ examples/2d_p_norm_q/src.py
@@ -1,5 +1,8 @@
 # %% load and generate data. Adapted from
 # https://pythonot.github.io/auto_examples/barycenters/plot_free_support_barycenter.html
+import sys
+sys.path.insert(1, '../../')
+
 import numpy as np
 import matplotlib.pylab as pl
 import matplotlib.pyplot as plt
@@ -11,6 +14,7 @@ from torch.optim import Adam
 from matplotlib import cm
 from tqdm import tqdm
 
+
 np.random.seed(42)
 torch.manual_seed(42)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -20,27 +24,31 @@ K = 3
 d = 2
 n = 400
 
-I1 = pl.imread("../data/redcross.png").astype(np.float64)[::4, ::4, 2]
-I2 = pl.imread("../data/duck.png").astype(np.float64)[::4, ::4, 2]
+I1 = pl.imread("../data/duck.png").astype(np.float64)[::4, ::4, 2]
+I2 = pl.imread("../data/redcross.png").astype(np.float64)[::4, ::4, 2]
 I3 = pl.imread("../data/heart.png").astype(np.float64)[::4, ::4, 2]
+
 
 sz = I2.shape[0]
 XX, YY = np.meshgrid(np.arange(sz), np.arange(sz))
 
-Y1 = np.stack((XX[I1 == 0], YY[I1 == 0]), 1) * 1.0
-Y2 = np.stack((XX[I2 == 0] + 80, -YY[I2 == 0] + 32), 1) * 1.0
-Y3 = np.stack((XX[I3 == 0] + 40, -YY[I3 == 0] + 80), 1) * 1.0
+Y1 = np.stack((XX[I1 == 0], -YY[I1 == 0]), 1) * 1.0
+Y2 = np.stack((XX[I2 == 0], -YY[I2 == 0] ), 1) * 1.0
+Y3 = np.stack((XX[I3 == 0], -YY[I3 == 0] ), 1) * 1.0
 Y1 = Y1 + np.random.randn(*Y1.shape) * .3
 Y2 = Y2 + np.random.randn(*Y2.shape) * .3
 Y3 = Y3 + np.random.randn(*Y3.shape) * .3
+Y2_visu = np.copy(Y2)+ np.array([80, 0])
+Y3_visu = np.copy(Y3)+ np.array([40, 60])
 
 Y_list = TT([Y1, Y2, Y3])
+Y_list_visu = TT([Y1, Y2_visu, Y3_visu])
 m_list = [Y.shape[0] for Y in Y_list]
 b_list = TT([ot.unif(m) for m in m_list])
 a = TT(ot.unif(n))
 
 plt.figure(1, (6, 6))
-for Y in Y_list:
+for Y in Y_list_visu:
     plt.scatter(*TN(Y.T), alpha=0.5)
 plt.title("Distributions")
 plt.axis('equal')
@@ -48,8 +56,8 @@ plt.axis('off')
 plt.tight_layout()
 
 # %% Define costs and visualise landscapes
-p_list = [1.5, 2, 3]  # p-norms
-q_list = [1.5, 2, 3]  # exponents to the p-norms
+p_list = [1,1.5,2,3]  # p-norms
+q_list = [1,1.5,2,3]  # exponents to the p-norms
 
 
 def p_norm_q_cost_matrix(u, v, p, q):
@@ -69,6 +77,7 @@ def C(x, y, p, q):
     return out
 
 
+#%%%%
 n_vis = 100
 u = torch.linspace(40, 80, n_vis, device=device)
 v = torch.linspace(0, 30, n_vis, device=device)
@@ -97,6 +106,8 @@ plt.show(block=True)
 
 
 # %% Define ground barycentre function B and test loss decrease
+
+
 def B(y, p, q, its=250, lr=1, log=False, stop_threshold=1e-20):
     """
     Computes the barycenter images for candidate points x (n, d) and
@@ -126,6 +137,7 @@ def B(y, p, q, its=250, lr=1, log=False, stop_threshold=1e-20):
         return x
 
 
+#%%%
 np.random.seed(42)
 torch.manual_seed(42)
 fig = plt.figure(figsize=(len(p_list) * 3, len(q_list) * 3))
@@ -190,9 +202,9 @@ plt.savefig('p_norm_q_barycentre_V.pdf')
 
 # %% Plot final iteration next to the measures
 plt.figure(1, (6, 4.5))
-for Y in Y_list:
+for Y in Y_list_visu:
     plt.scatter(*TN(Y.T), alpha=0.5)
-plt.scatter(*TN(X_bar.T), alpha=0.5)
+plt.scatter(*TN(X_bar.T)+np.array([[40], [20]]), alpha=0.5)
 plt.title("Barycentre for the cost $|x-y|_{3/2}^{3/2}$", fontsize=16)
 plt.axis('equal')
 plt.axis('off')
@@ -242,110 +254,4 @@ for p_idx, p in enumerate(p_list):
         ax.set_title(f'p={p}, q={q}', fontsize=12)
 plt.savefig('p_norm_q_barycentres_V.pdf')
 
-# %% with extra outliers
-np.random.seed(42)
-torch.manual_seed(42)
-n_outliers = 30
-outliers_scale = 10
-Y_list_outliers = []
-for Y in Y_list:
-    noise = torch.randn(n_outliers, d, device=device) * outliers_scale
-    noise += torch.mean(Y, axis=0)[None, :]
-    Y_outliers = torch.cat([Y, noise], axis=0)
-    Y_list_outliers.append(Y_outliers)
-b_list_outliers = TT([ot.unif(Y.shape[0]) for Y in Y_list_outliers])
 
-its_outliers = 10
-p, q = 1.5, 1.5
-cost_list = [lambda x, y: p_norm_q_cost_matrix(x, y, p, q)] * K
-its = 2
-X_bar, log_dict = solve_OT_barycenter_fixed_point(
-    X_init, Y_list_outliers, b_list_outliers, cost_list, lambda y: B(y, p, q),
-    max_its=its_outliers, log=True, stop_threshold=0.)
-
-plt.figure(1, (6, 4.5))
-for Y in Y_list_outliers:
-    plt.scatter(*TN(Y.T), alpha=0.5)
-plt.scatter(*TN(X_bar.T), alpha=0.5)
-plt.title("Barycentre for the cost $|x-y|_{3/2}^{3/2}$", fontsize=16)
-plt.axis('equal')
-plt.axis('off')
-plt.tight_layout()
-plt.savefig('p_norm_q_barycentre_outliers.pdf')
-
-# %% barycentre grid for outliers
-np.random.seed(0)
-torch.manual_seed(0)
-X_bar_dict_outliers = {}
-X_bar_list_dict_outliers = {}
-for p in tqdm(p_list):
-    for q in q_list:
-        cost_list = [lambda x, y: p_norm_q_cost_matrix(x, y, p, q)] * K
-        X_bar, log_dict = solve_OT_barycenter_fixed_point(
-            X_init, Y_list_outliers, b_list_outliers, cost_list,
-            lambda y: B(y, p, q), max_its=its_outliers, log=True,
-            stop_threshold=stop_threshold)
-        X_bar_dict_outliers[(p, q)] = X_bar
-        X_bar_list_dict_outliers[(p, q)] = log_dict['X_list']
-
-# %% plot barycentres for outliers
-fig = plt.figure(figsize=(len(p_list) * 3, len(q_list) * 3))
-for p_idx, p in enumerate(p_list):
-    for q_idx, q in enumerate(q_list):
-        i = p_idx * len(q_list) + q_idx
-        ax = fig.add_subplot(len(p_list), len(q_list), i + 1)
-        X_bar = X_bar_dict_outliers[(p, q)]
-        ax.scatter(*TN(X_bar.T), alpha=0.5)
-        ax.set_title(f'p={p}, q={q}', fontsize=12)
-        ax.axis('equal')
-        ax.axis('off')
-plt.tight_layout()
-plt.savefig('p_norm_q_barycentres_grid_outliers.pdf')
-
-# %% outlier experiments with 2D gaussians
-np.random.seed(42)
-torch.manual_seed(42)
-
-K = 2
-d = 2
-n = 100  # barycentre size, measures will be 2n + n_outliers
-n_outliers = 10
-
-Y_list = []
-offsets = [torch.tensor([0, 0], device=device, dtype=torch.double),
-           torch.tensor([5, 0], device=device, dtype=torch.double)]
-for k in range(K):
-    Y = torch.rand(2 * n, d, device=device, dtype=torch.double) - .5
-    outliers = \
-        torch.randn(n_outliers, d, device=device, dtype=torch.double) * .75
-    Y = torch.cat([Y, outliers], axis=0)
-    Y_list.append(Y + offsets[k][None, :])
-
-b_list = TT([ot.unif(Y.shape[0]) for Y in Y_list])
-X_init = torch.rand(n, d, device=device, dtype=torch.double)
-
-fixed_point_its = 5
-stop_threshold = 1e-10
-q_list = [1.1, 2]
-p = 2
-X_bar_dict = {}
-X_bar_list_dict = {}
-for q in q_list:
-    cost_list = [lambda x, y: p_norm_q_cost_matrix(x, y, p, q)] * K
-    X_bar, log_dict = solve_OT_barycenter_fixed_point(
-        X_init, Y_list, b_list, cost_list, lambda y: B(y, p, q), max_its=fixed_point_its, log=True, stop_threshold=stop_threshold)
-    X_bar_dict[q] = X_bar
-    X_bar_list_dict[q] = log_dict['X_list']
-
-fig = plt.figure(figsize=(6, len(q_list) * 3))
-for q_idx, q in enumerate(q_list):
-    ax = fig.add_subplot(len(q_list), 1, q_idx + 1)
-    X_bar = X_bar_dict[q]
-    for Y in Y_list:
-        ax.scatter(*TN(Y.T), alpha=0.5)
-    ax.scatter(*TN(X_bar.T), alpha=0.5)
-    ax.set_title(f'p={p}, q={q}', fontsize=12)
-    ax.axis('equal')
-    ax.axis('off')
-plt.tight_layout()
-plt.savefig('2_norm_q_outliers.pdf')
